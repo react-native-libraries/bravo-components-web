c1bf860aa78532e3bf9d099c0fe9155c6880088c
Matheus Cardoso <matheusqcardoso98@gmail.com>
2021-10-14 13:49:30 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Btn from './application/bravo-components/components/all';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Btn backgroundColor="#000" label="Clique aqui!" padding={25} />
     </View>
   );
 }
