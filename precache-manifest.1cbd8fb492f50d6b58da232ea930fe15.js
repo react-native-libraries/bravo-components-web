c6988d7d2b954ed5b0b7a8421be7ae2ebdd89f75
Matheus Quirino Cardoso <matheus.quirino@bravolog.com.br>
2021-10-14 10:55:24 -0300

    Updates

---
+++ precache-manifest.1cbd8fb492f50d6b58da232ea930fe15.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bf1f2ac1233375f9dd2f42f724ff36",
+    "url": "/react-native-web/index.html"
+  },
+  {
+    "revision": "5ba31935f6dd9e1b8388",
+    "url": "/react-native-web/static/css/2.800b4633.chunk.css"
+  },
+  {
+    "revision": "9d18c2870ab45f46dda9",
+    "url": "/react-native-web/static/css/main.4609059e.chunk.css"
+  },
+  {
+    "revision": "5ba31935f6dd9e1b8388",
+    "url": "/react-native-web/static/js/2.7afd3cad.chunk.js"
+  },
+  {
+    "revision": "3e76bf0548099860c044e66aa2f76b1d",
+    "url": "/react-native-web/static/js/2.7afd3cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d18c2870ab45f46dda9",
+    "url": "/react-native-web/static/js/main.b0c322ef.chunk.js"
+  },
+  {
+    "revision": "c5eafabe4b2314660f3de33fe2c9484b",
+    "url": "/react-native-web/static/js/main.b0c322ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4112a67ac82c28f493f8",
+    "url": "/react-native-web/static/js/runtime-main.63899609.js"
+  },
+  {
+    "revision": "53504bb75f49ce395a3a8b7787f0e342",
+    "url": "/react-native-web/static/media/Evolog_Motorista_Overview.53504bb7.mp4"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/react-native-web/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/react-native-web/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/react-native-web/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/react-native-web/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/react-native-web/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/react-native-web/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/react-native-web/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
